22cdf8f7cb138f4c8abda643db8834c0486aeac6
muratmnz <minazzmurat@gmail.com>
2023-03-05 13:28:42 +0300

    Initial Commit

---
+++ AskMeApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AskMeApp
+//
+//  Created by Murat Minaz on 5.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
