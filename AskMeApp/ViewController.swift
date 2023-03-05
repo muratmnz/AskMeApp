3977b7a5534d11360d4495442f4262b7f15b3225
muratmnz <minazzmurat@gmail.com>
2023-03-05 14:59:51 +0300

    feature :  added ask magic ball array and ask button works random asks

--- AskMeApp/ViewController.swift
+++ AskMeApp/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let askBalls = ["ball1","ball2","ball3","ball4","ball5"]
+    
+    @IBOutlet weak var askQsBallsView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        askQsBallsView.alpha = 0.0
     }
 
-
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        askQsBallsView.alpha = 1.0
+        askQsBallsView.image = UIImage(imageLiteralResourceName: askBalls.randomElement()!)
+        
+    }
+    
+    
 }
 
